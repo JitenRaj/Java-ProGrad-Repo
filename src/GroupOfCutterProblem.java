5a4b4ac25b9464594a8d01bbf2bc8ff18c5c41b3
Jitendra Singh Rajpurohit <Jitendra.Rajpurohit@ivycomptech.com>
2022-09-14 16:40:42 +0530

    Array of Strings

---
+++ src/GroupOfCutterProblem.java
@@ -0,0 +1,11 @@
+public class GroupOfCutterProblem {
+
+    public static void main(String[] args) {
+
+        String[] groupOfCutter = new String[]{ "nailCutter", "cookieCutter",
+                "cableCutter", "vegetableCutters", "microCutter"} ;
+
+        for(String cutter : groupOfCutter)
+            System.out.println( cutter );
+    }
+}
