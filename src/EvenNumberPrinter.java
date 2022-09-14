db0b2dac534dd48bff4a971b376595f219d37ace
Jitendra Singh Rajpurohit <Jitendra.Rajpurohit@ivycomptech.com>
2022-09-14 16:43:30 +0530

    Even number Printer

--- src/EvenNumberPrinter.java
+++ src/EvenNumberPrinter.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class EvenNumberPrinter {
+public class EvenNumberPrinter {
+    public static void main(String[] args) {
+
+        printUsingForLoop( 20 );
+
+        printUsingForEachLoop( 15);
+    }
+
+    public static void printUsingForLoop(int size) {
+
+        System.out.println("Using For Loop : ");
+
+        for(int i = 0; i<size; i++){
+            if( i % 2 == 0)
+                System.out.print( i + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void printUsingForEachLoop( int size) {
+
+        System.out.println( "Using ForEach Loop : ");
+
+        int[] intArray = new int[size];
+        for(int i=0; i<size; i++) {
+            intArray[i] = i;
+        }
+
+        for ( int i : intArray ) {
+            if( i % 2 == 0)
+                System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
 }
